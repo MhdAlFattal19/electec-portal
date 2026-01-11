37ea410f94295d1947869c6741080fa14ee71f05
MhdAlFattal <Mohammad.Moafak@aventura.com>
2026-01-11 14:47:20 +0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-/**
- * @license
- * Copyright Akveo. All Rights Reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- */
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { CoreModule } from './@core/core.module';
-import { ThemeModule } from './@theme/theme.module';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+
+import { CoreModule } from './@core/core.module';
+import { ThemeModule } from './@theme/theme.module';
+import { AuthModule } from './auth/auth.module';
+import { SharedModule } from './shared/shared.module';
+
 import {
   NB_LAYOUT_DIRECTION,
   NbChatModule,
@@ -22,42 +23,40 @@ import {
   NbToastrModule,
   NbWindowModule,
 } from '@nebular/theme';
-import { RouterModule } from '@angular/router';
-import { routes } from '@nebular/auth';
-import { AuthModule } from './auth/auth.module';
+
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { ToastrModule } from 'ngx-toastr';
-import { SharedModule } from './shared/shared.module';
+
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { environment } from '../environments/environment';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    RouterModule.forRoot(routes),
     HttpClientModule,
+
     AppRoutingModule,
+
     NbSidebarModule.forRoot(),
     NbMenuModule.forRoot(),
     NbDatepickerModule.forRoot(),
     NbDialogModule.forRoot(),
     NbWindowModule.forRoot(),
     NbToastrModule.forRoot(),
-    NbChatModule.forRoot({
-      messageGoogleMapKey: 'AIzaSyA_wNuCzia92MAmdLRzmqitRGvCF7wCZPY',
-    }),
     CoreModule.forRoot(),
     ThemeModule.forRoot(),
-    ToastrModule.forRoot({
-      positionClass: "toast-top-center"
-    }),
     AuthModule,
-    NgxSpinnerModule,
     SharedModule,
-    NbToastrModule.forRoot(),
+
+    ToastrModule.forRoot({
+      positionClass: 'toast-top-center',
+    }),
+
+    NgxSpinnerModule,
+
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
   ],
@@ -67,5 +66,4 @@ import { HashLocationStrategy, LocationStrategy } from '@angular/common';
   ],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
